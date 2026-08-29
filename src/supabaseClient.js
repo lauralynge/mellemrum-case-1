27425ec0fabfa912995b04f593439bf2de5a2339
lauralynge <lauralynge@gmail.com>
2026-08-29 10:46:30 +0200

    refactor: add shared supabaseClient with URL and headers

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+
+export const headers = {
+  apikey: import.meta.env.VITE_SUPABASE_APIKEY,
+  "Content-Type": "application/json",
+};
